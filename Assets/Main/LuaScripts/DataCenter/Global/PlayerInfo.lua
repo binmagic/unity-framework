04ca763fd5420a9bbff983c7feed946b9f35b58d
bianxueming <12312577@qq.com>
2026-07-15 14:46:41 +0800

    增加玩家存档

---
+++ Assets/Main/LuaScripts/DataCenter/Global/PlayerInfo.lua
@@ -0,0 +1,13 @@
+---@class PlayerInfo
+local PlayerInfo = BaseClass("PlayerInfo")
+
+local function __init(self)
+end
+
+local function __delete(self)
+end
+
+PlayerInfo.__init = __init
+PlayerInfo.__delete = __delete
+
+return PlayerInfo
